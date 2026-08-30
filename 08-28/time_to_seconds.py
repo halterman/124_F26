1bee15c5354655726ae083000155667949acc0a0
halterman <halterman@southern.edu>
2026-08-30 19:33:17 -0400

    Added the beginning of a time conversion function.

---
+++ 08-28/time_to_seconds.py
@@ -0,0 +1,8 @@
+print("Please enter hours, minutes, and seconds: ")
+hours = int(input())
+minutes = int(input())
+seconds = int(input())
+
+print('The total number of seconds are: ')
+total_seconds = hours * 3600 + minutes * 60 + seconds
+print(total_seconds)
