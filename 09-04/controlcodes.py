81ade6e51e7d3403053b3861fea904e2e9c5e3a6
halterman <halterman@southern.edu>
2026-09-04 12:10:54 -0400

    Floating-point and escape characters

---
+++ 09-04/controlcodes.py
@@ -0,0 +1,13 @@
+word1 = "Pro\ngramming"
+word2 = "Pro\rgramming"
+word3 = "Pro\tgramming"
+word4 = "Pro\bgramming"
+word5 = "Pro\agramming"
+word6 = "Pro\"agramming"
+
+print(word1)
+print(word2)
+print(word3)
+print(word4)
+print(word5)
+print(word6)
---
+++ 09-04/precise_eighths.py
@@ -0,0 +1,5 @@
+one = 1.0
+one_eighth = 1.0/8.0
+zero = one - one_eighth - one_eighth - one_eighth - one_eighth \
+           - one_eighth - one_eighth - one_eighth - one_eighth
+print('one =', one, ' one-eighth =', one_eighth, ' zero =', zero)
