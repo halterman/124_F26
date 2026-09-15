c454d959806ff5f0f8f859ed3946c9cd50bf4d89
halterman <halterman@southern.edu>
2026-09-15 10:22:31 -0400

    Corrected prompt strings in max of 5 program

--- 09-16/maxof5.py
+++ 09-16/maxof5.py
@@ -1,9 +1,9 @@
 # Get the user's input
 print('Please enter five integers')
 num1 = int(input('#1 ==> '))
-num1 = int(input('#2 ==> '))
-num1 = int(input('#3 ==> '))
-num1 = int(input('#4 ==> '))
-num1 = int(input('#5 ==> '))
+num2 = int(input('#2 ==> '))
+num3 = int(input('#3 ==> '))
+num4 = int(input('#4 ==> '))
+num5 = int(input('#5 ==> '))
 
 # Determine the largest number entered
